91530b223f659013091262b0831875989bff083b
simi1912 <nioasca@gmail.com>
2021-12-30 17:40:04 +0200

    Added Product and ProductCategory dao.

--- src/main/java/com/shop/ECommerceBackendApplication.java
+++ src/main/java/com/shop/ECommerceBackendApplication.java
@@ -2,6 +2,7 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class ECommerceBackendApplication {
--- src/main/java/com/shop/dao/ProductRepository.java
+++ src/main/java/com/shop/dao/ProductRepository.java
@@ -15,7 +15,9 @@ package com.shop.dao;
 
 import com.shop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 
